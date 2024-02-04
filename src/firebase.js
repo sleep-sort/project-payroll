1348e5ea6f58a2d1c657369c1e61d0a2b72f0135
mattwbdv <koenigmattd@gmail.com>
2024-02-04 13:33:05 -0800

    initial

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBs-pxxCqj5Qjp7aHjyqO1SG1k2GRUuQCo",
+  authDomain: "project-payroll-fb247.firebaseapp.com",
+  projectId: "project-payroll-fb247",
+  storageBucket: "project-payroll-fb247.appspot.com",
+  messagingSenderId: "167662014094",
+  appId: "1:167662014094:web:1cb6f234c398ca01a743df"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
